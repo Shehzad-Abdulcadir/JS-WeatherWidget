ed857f573423910762c666abc3628ac52d369d98
Shehzad Abdulcadir <Shehzad-Abdulcadir@users.noreply.github.com>
2019-01-15 09:58:47 +0000

    [JS] - Create variables + get api request

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,9 @@
 import '../styles/index.scss';
 
-console.log('Test');
+const darkSkyApiKey = "a257110504dbfc792d2cd40c7ad8a0f7";
+const darkSkyApiLat = "52.372600";
+const darkSkyApiLong = "-1.246700";
+const darkSkyApiRequestURL = "https://api.darksky.net/forecast/" + darkSkyApiKey + "/" + darkSkyApiLat + "," + darkSkyApiLong;
+//https://api.darksky.net/forecast/[key]/[latitude],[longitude]
+
+console.log(darkSkyApiRequestURL);
