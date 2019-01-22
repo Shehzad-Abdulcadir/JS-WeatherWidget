42b23c4547374c43af33ab3182d1667048c98f10
Shehzad Abdulcadir <Shehzad-Abdulcadir@users.noreply.github.com>
2019-01-22 10:06:36 +0000

    [JS] - Add ES6 template strings + Cleanup HTML

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,9 +1,27 @@
-import '../styles/index.scss';
+import "../styles/index.scss";
 
 const darkSkyApiKey = "a257110504dbfc792d2cd40c7ad8a0f7";
 const darkSkyApiLat = "52.372600";
 const darkSkyApiLong = "-1.246700";
-const darkSkyApiRequestURL = "https://api.darksky.net/forecast/" + darkSkyApiKey + "/" + darkSkyApiLat + "," + darkSkyApiLong;
+const darkSkyApiRequestURL = `https://api.darksky.net/forecast/${darkSkyApiKey}/${darkSkyApiLat},${darkSkyApiLong};`;
 //https://api.darksky.net/forecast/[key]/[latitude],[longitude]
 
-console.log(darkSkyApiRequestURL);
+var request = new XMLHttpRequest();
+request.open("GET", darkSkyApiRequestURL, true);
+
+request.onload = function() {
+    if (this.status >= 200 && this.status < 400) {
+    // Success!
+        var data = JSON.parse(this.response);
+    } else {
+        // We reached our target server, but it returned an error
+    }
+};
+
+request.onerror = function() {
+    // There was a connection error of some sort
+};
+
+request.send();
+
+console.log(request);
