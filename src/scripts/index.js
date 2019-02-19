4887b2feb565c4f419b201c870d85b13d394e19f
Shehzad Abdulcadir <Shehzad-Abdulcadir@users.noreply.github.com>
2019-02-19 10:32:25 +0000

    [JS] - Force response to use celsius

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -7,6 +7,8 @@ const darkSkyApiLong = "-1.246700";
 const proxy = 'https://cors-anywhere.herokuapp.com/';
 // https://api.darksky.net/forecast/[key]/[latitude],[longitude]
 const darkSkyApiRequestURL = proxy + `https://api.darksky.net/forecast/${darkSkyApiKey}/${darkSkyApiLat},${darkSkyApiLong}`;
+// Add Concat function to add additional paramaters to API request
+darkSkyApiRequestURL.concat('?units=uk2');
 
 var request = new XMLHttpRequest();
 request.open("GET", darkSkyApiRequestURL, true);
@@ -15,15 +17,16 @@ request.onload = function() {
     if (this.status >= 200 && this.status < 400) {
     // Success!
         var data = JSON.parse(this.response);
+        console.log(data);
     } else {
         // We reached our target server, but it returned an error
+        console.log('The API request returned an error.');
     }
 };
 
 request.onerror = function() {
     // There was a connection error of some sort
+    console.log('Connection error');
 };
 
 request.send();
-
-console.log(request);
